a78763d40a93a566471d4804ae008ffd47f3f040
Niel Conradie <nielconradie93@gmail.com>
2022-05-30 16:43:59 +0200

    Update hangman.py
    Object-Oriented Programming Implementation.

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,61 +1,71 @@
 import random
-import string
 
-from words import words
 from hangman_visual import lives_visual_dict
 
 
-def random_word(words):
-    """ Randomly select a word from a list. """
-    word = random.choice(words)
-    while '-' in word or ' ' in word:
+class Hangman:
+    """ A class to represent a Hangman game. """
+
+    def __init__(self):
+        """ Initialize class attributes. """
+        self.lives = 6
+
+    @staticmethod
+    def random_word(words):
+        """ Randomly select a word from a list. """
         word = random.choice(words)
-    return word.upper()
+        while '-' in word or ' ' in word:
+            word = random.choice(words)
+        return word.upper()
 
+    def display_hangman(self):
+        """ Display hangman visuals. """
+        print(lives_visual_dict[self.lives])
 
-def play():
-    """ Hangman. """
-    word = random_word(words)
-    word_letters = set(word)
-    alphabet = set(string.ascii_uppercase)
-    used_letters = set()
-
-    lives = 6
-    while len(word_letters) > 0 and lives > 0:
-        # Display letters used and lives left.
-        print(f"\nLives: {lives}")
+    def display_text(self, word, used_letters):
+        """ Display hangman information. """
+        print(f"Lives: {self.lives}")
         print("You have used these letters: ", ' '.join(used_letters))
-
-        # Display current word and hangman.
         word_list = [
             letter if letter in used_letters else '-' for letter in word]
-        print(lives_visual_dict[lives])
         print("Current word: ", ' '.join(word_list))
 
-        # Requesting player input.
-        player_input = input("Guess a letter: ").upper()
+    @staticmethod
+    def user_input_guess():
+        """ Requesting user input and validating letter. """
+        while True:
+            try:
+                user_input = input("Guess a letter: ").upper()
+            except ValueError:
+                print(f"\nThat is not a letter.")
+                continue
+            return user_input
 
-        # Player input validation and conditions.
-        if player_input in alphabet - used_letters:
-            used_letters.add(player_input)
-            if player_input in word_letters:
-                word_letters.remove(player_input)
+    def validate_user_guess(self, user_input_guess, word_letters, alphabet, used_letters):
+        """ User input validation and conditions. """
+        if user_input_guess in alphabet - used_letters:
+            used_letters.add(user_input_guess)
+            if user_input_guess in word_letters:
+                word_letters.remove(user_input_guess)
             else:
-                lives -= 1
-                print(f"\n{player_input} is not in the word.")
-        elif player_input in used_letters:
+                self.lives -= 1
+                print(f"\n{user_input_guess} is not in the word.")
+        elif user_input_guess in used_letters:
             print("\nYou have already used that character. Please try again.")
         else:
             print("\nInvalid character. Please try again.")
 
-    # Game Won/Lost conditions.
-    if lives == 0:
-        print(lives_visual_dict[lives])
-        print(f"You have died. The word was {word}. Better luck next time!")
-    else:
-        print("\nCongratulations!")
-        print(f"You have guessed the word {word} correctly!")
+    def dead_condition(self, word):
+        """ Return True if out of lives. """
+        if self.lives == 0:
+            print(
+                f"\nYou have died. The word was {word}. Better luck next time!")
+            return True
 
-
-if __name__ == '__main__':
-    play()
+    @staticmethod
+    def win_condition(word, word_letters):
+        """ Return True if correct word. """
+        if len(word_letters) == 0:
+            print("\nCongratulations!")
+            print(f"You have guessed the word {word} correctly!")
+            return True
