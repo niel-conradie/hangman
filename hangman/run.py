64a65833b06730fc7b35503e8aed83304f712a29
Niel Conradie <nielconradie93@gmail.com>
2022-06-14 16:03:49 +0200

    Update run.py
    Added restart method to run.py file.

--- hangman/run.py
+++ hangman/run.py
@@ -8,29 +8,47 @@ def run():
     """ Hangman. """
     run = Hangman()
 
-    word = run.random_word(words)
-    word_letters = set(word)
-    alphabet = set(string.ascii_uppercase)
-    used_letters = set()
-
     while True:
-        # Display hangman visuals.
-        run.display_hangman()
-        # Display hangman text.
-        run.display_text(word, used_letters)
+        word = run.random_word(words)
+        word_letters = set(word)
+        alphabet = set(string.ascii_uppercase)
+        used_letters = set()
+
+        while True:
+            # Display hangman visuals.
+            run.display_hangman()
+
+            # Display hangman text.
+            run.display_text(word, used_letters)
+
+            # Requesting user input.
+            user_input_guess = run.user_input_guess()
+
+            # Validating user input and conditions.
+            run.validate_user_guess(
+                user_input_guess, word_letters, alphabet, used_letters)
+
+            # Out of lives condition.
+            if run.dead_condition(word) == True:
+                # Display hangman visuals.
+                run.display_hangman()
+
+                # Reset lives.
+                run.reset_player_lives()
+                break
+
+            # Correct word condition.
+            if run.win_condition(word, word_letters) == True:
+                # Reset lives.
+                run.reset_player_lives()
+                break
+
+            continue
+
         # Requesting user input.
-        user_input_guess = run.user_input_guess()
-        # Validating user input and conditions.
-        run.validate_user_guess(
-            user_input_guess, word_letters, alphabet, used_letters)
+        run.restart()
 
-        # Out of lives condition.
-        if run.dead_condition(word) == True:
-            break
-
-        # Correct word condition.
-        if run.win_condition(word, word_letters) == True:
-            break
+        continue
 
 
 if __name__ == '__main__':
