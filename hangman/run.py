fc29da64a956fe5b542368ff385d457b31afcca1
Niel Conradie <nielconradie93@gmail.com>
2023-01-04 17:13:11 +0200

    Update run.py
    Import optimization

--- hangman/run.py
+++ hangman/run.py
@@ -1,4 +1,4 @@
-import sys
+from sys import exit
 
 from hangman import Hangman
 
@@ -12,7 +12,7 @@ def run():
         run.start_game()
     except KeyboardInterrupt:
         # Stopping the game.
-        sys.exit("\n\nProgram Terminated")
+        exit("\n\nProgram Terminated")
 
 
 if __name__ == "__main__":
